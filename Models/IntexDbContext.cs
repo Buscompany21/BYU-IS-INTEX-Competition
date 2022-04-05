3a608749fb4e8902df41a664c9dccf43c95f028b
Buscompany21 <buscobrian1@gmail.com>
2022-04-04 18:54:59 -0600

    working on intexdbcontext

---
+++ Models/IntexDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INTEX2.Models
+{
+    public class IntexDbContext : DbContext
+    {
+        public IntexDbContext(DbContext<IntexDbContext> options) : base(options)
+        {
+
+
+        }
+
+        public DbSet ChangeThis { get; set; }
+    }
+}
