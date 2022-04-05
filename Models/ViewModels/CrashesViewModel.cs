83c2223d27f462fcae4978121994cef4b83fabc1
Buscompany21 <buscobrian1@gmail.com>
2022-04-05 15:11:12 -0600

    pagination

---
+++ Models/ViewModels/CrashesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INTEX2.Models.ViewModels
+{
+    public class CrashesViewModel
+    {
+        public IQueryable<Crash> Crashes { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
